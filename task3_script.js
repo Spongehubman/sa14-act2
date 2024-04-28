8f768e1b73e2ec832b950f32d59fc88e17489b12
Nicholas Folis <heffawoozle77@gmail.com>
2024-04-27 21:42:25 -0500

    I modified 'task1.html' so that the 'submit' parameter of the 'async function' function was changed into 'click', and more progress was made inside of the '<script>' tag to log the retrieved current date to the console, and to try to print out the retireved data. The 'onsubmit=''return false''' parameter of the '<form>' tag was also commented out. 'task3.html' had its '<div>' tag transformed into an unordered list tag. In 'task3_script.js', the 'document.createElement(''<br>'')' statement was removed, and new code was added to add a child node instead. Also, the code to change the 'innerHTML' of the 'task3.html' element with the id 'output' was removed as well.

--- task3_script.js
+++ task3_script.js
@@ -5,14 +5,16 @@ document.getElementById("submitAdd").addEventListener("click", add)
 function add() {
     event.preventDefault()
     var task = document.getElementById("title").value
-    const breaker = document.createElement("br")
+    const item = document.createElement("li")
+    const name = document.createTextNode(task)
 
     console.log(task)
     //responseElement.innerElement = task.value
 
     // Obtain the ID of the target element to output.
 
-    let outputElement = document.getElementById("output")
-    outputElement.innerHTML += task + breaker.innerHTML
+    item.appendChild(name)
+    document.getElementById("output").appendChild(name)
+
 
 }
